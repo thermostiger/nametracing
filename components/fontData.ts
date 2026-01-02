f1844255ab257d563ca39d2bdcdbe8b9b7b89c24
thermostiger <x_in@outlook.com>
2026-01-03 03:35:44 +0800

    1

---
+++ components/fontData.ts
@@ -0,0 +1,3 @@
+// This file is dedicated to holding the large Base64 string for the custom font.
+// Paste your Base64 string inside the quotes below. Do not add newlines.
+export const customFontBase64 = ""; 
